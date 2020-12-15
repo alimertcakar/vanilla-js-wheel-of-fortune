48148a11db27d90d4d1927a430dd7e0bf1507e86
Ali Mert Çakar <mail@alimert.net>
2020-12-15 17:52:57 +0300

    final

--- vanilla-js-wheel-of-fortune-FINISHED/script.js
+++ vanilla-js-wheel-of-fortune-FINISHED/script.js
@@ -1,35 +1,47 @@
-// Immediately invoked function expression
-// to not pollute the global scope
-(function() {
+function randomIntFromInterval(min, max) { // min max arasında sayı - min max dahil
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function fortuneStep(step) {
+  return ((step - 1) * 45);
+}
+
+
+
+
+
+(function () {
   const wheel = document.querySelector('.wheel');
   const startButton = document.querySelector('.button');
   let deg = 0;
-
   startButton.addEventListener('click', () => {
-    // Disable button during spin
     startButton.style.pointerEvents = 'none';
-    // Calculate a new rotation between 5000 and 10 000
-    deg = Math.floor(5000 + Math.random() * 5000);
-    // Set the transition on the wheel
-    wheel.style.transition = 'all 10s ease-out';
-    // Rotate the wheel
+    deg = Math.floor(5040 + fortuneStep(1) + 1 + randomIntFromInterval(0, 44));
+    wheel.style.transition = 'all 1s ease-out';
     wheel.style.transform = `rotate(${deg}deg)`;
-    // Apply the blur
     wheel.classList.add('blur');
   });
 
   wheel.addEventListener('transitionend', () => {
-    // Remove blur
     wheel.classList.remove('blur');
-    // Enable button when spin is over
     startButton.style.pointerEvents = 'auto';
-    // Need to set transition to none as we want to rotate instantly
     wheel.style.transition = 'none';
-    // Calculate degree on a 360 degree basis to get the "natural" real rotation
-    // Important because we want to start the next spin from that one
-    // Use modulus to get the rest value from 360
-    const actualDeg = deg % 360;
-    // Set the real rotation instantly without animation
-    wheel.style.transform = `rotate(${actualDeg}deg)`;
   });
 })();
+
+
+function cevirBeni(carkAdimi) {
+  const wheel = document.querySelector('.wheel');
+  const startButton = document.querySelector('.button');
+  let deg = 0;
+
+  startButton.style.pointerEvents = 'none';
+  deg = Math.floor(5040 + fortuneStep(carkAdimi) + 1 + randomIntFromInterval(0, 44));
+  wheel.style.transition = 'all 10s ease-out';
+  wheel.style.transform = `rotate(${deg}deg)`;
+  wheel.classList.add('blur');
+
+  //Çarkı eski yerine döndürme
+  //const actualDeg = deg % 360;
+  //wheel.style.transform = `rotate(${actualDeg}deg)`;
+}
